8e4b6bff539a8bd191b549de819452423c002764
Abhinav Venkat <Asivakumargayat1253@conestogac.on.ca>
2025-10-28 11:24:58 -0400

    test

---
+++ app/app.js
@@ -0,0 +1,20 @@
+
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello from React Native </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+});
