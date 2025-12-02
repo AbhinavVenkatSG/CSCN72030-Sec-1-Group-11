c73703fd0bfb1440375e268863aae774c5736a6e
Yullitsael <Ricardodpinedap@gmail.com>
2025-12-02 13:43:01 -0500

    Added Dosimeter unit tests
    MEOW

--- Tests/Backend/IDeviceTests/DeviceFileReadTests.cs
+++ Tests/Backend/IDeviceTests/DeviceFileReadTests.cs
@@ -11,7 +11,7 @@ namespace IDeviceTests
         private readonly BunkerStatuses bunkerStatuses = new();
 
 
-        //Verify that the returned DeviceStatus.currentValue matches the value in the .dat file.
+        //Verify that the Thermometer is reading the right values from the right file
         [TestMethod]
         public void Thermometer_QueryLatestTest()
         {
@@ -22,11 +22,11 @@ namespace IDeviceTests
             temp = t.QueryLatest();
             //Assert
             Assert(temp == 22);
-        } //I only tested the 1st of 3 unit tests because they are all asking to test the same thing in the same way.
+        }
 
-        // Confirm bunkerStatuses.Temperature is updated after calling QueryLatest().
+        //Confirm bunkerStatuses.Temperature is updated after calling QueryLatest().
         //I'll just run query latest twice and make sure that its giving the right 2 values.
-        public void Thermometer_QueryLatestTest()
+        public void Thermometer_UpdateTest()
         {
             //Arrange
             Thermometer t = new Thermometer();
@@ -38,6 +38,35 @@ namespace IDeviceTests
             //Assert
             Assert(temp1 == 22);
             Assert(temp2 == 24);
+        }
+
+
+        //Verify that the Dosimeter is reading the right values from the right file
+        [TestMethod]
+        public void Dosimeter_QueryLatestTest()
+        {
+            //Arrange
+            Dosimeter d = new Dosimeter();
+            int rad;
+            //Act
+            rad = d.QueryLatest();
+            //Assert
+            Assert(rad == 45);
+        } 
+
+        //Verify that the Dosimeter is updating properly after every day and reading the right line of the file
+        public void Dosimeter_QueryLatestTest()
+        {
+            //Arrange
+            Dosimeter d = new Dosimeter();
+            int rad1;
+            int rad2;
+            //Act
+            rad1 = t.QueryLatest();
+            rad2 = t.QueryLatest();
+            //Assert
+            Assert(rad1 == 45);
+            Assert(rad2 == 4);
         }
     }
 }
