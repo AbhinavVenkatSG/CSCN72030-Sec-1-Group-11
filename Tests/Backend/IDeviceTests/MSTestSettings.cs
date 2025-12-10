468948f6dcbea83a1a4b1beda0e61f0bd1870b0a
Abhinav Venkat <Asivakumargayat1253@conestogac.on.ca>
2025-12-10 04:18:36 -0500

    Penultimate commit

--- Tests/Backend/IDeviceTests/MSTestSettings.cs
+++ Tests/Backend/IDeviceTests/MSTestSettings.cs
@@ -1,5 +1,1 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-
-// This line forces all tests to run sequentially (one by one)
-// to prevent them from fighting over the "SensorEmulationFiles" folder.
-[assembly: DoNotParallelize]
+﻿[assembly: DoNotParallelize]
